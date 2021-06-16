0bef56c998bf1b63206241122dbfa8aaea3f41b8
Alex Vong <avong003@gmail.com>
2021-06-15 17:07:03 -0700

    footer.php

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer>
+	<section class="call-to-action">
+		<div class="inner-column">
+			<section class="cta-module">
+				<h2 class="header">This is the site footer and it has a &#34;site-map&#34; module in it</h2>
+				<p class="support-text">
+					Pretty standard type of thing, right?
+				</p>
+		
+			</section>
+		</div>
+	</section>
+
+
+
+	<section class="footer-menu-options">
+		<section class="footer-logo">
+			<div class="inner-column">
+				<h3 class="">Travel&nbsp; <i class="fas fa-plane-departure"></i></h3>
+			</div>
+	    </section>
+
+
+
+		<div class="inner-column">
+			<div class="nav-box">
+				<a href="#">Logo</a>
+				<a href="#">Some page</a>
+				<a href="#">Some other page</a>			
+			</div>
+
+			<div class="nav-box">
+				<a href="#">Sign-in</a>
+			</div>
+
+			<div class="nav-box">
+				<a href="#">Legal thing a</a>
+				<a href="#">Legal thing a</a>
+			</div>
+
+			<div class="nav-box">
+				<a href="#">Social thing a</a>
+				<a href="#">Social thing b</a>
+				<a href="#">Social thing c</a>
+				<a href="#">Social thing d</a>
+			</div>
+		</div>
+	</section>
+</footer>
