ad7f5d7cf16bce32899f4698be39ae79d32cd4dc
Alex Vong <avong003@gmail.com>
2021-06-15 17:07:12 -0700

    header

---
+++ header.php
@@ -0,0 +1,14 @@
+
+<div class="inner-column">
+	<masthead>
+		<nav class="menu-options">
+			<a href="#" class="site-logo header">Travel&nbsp;<i class="fas fa-plane-departure"></i></a>
+			<a href="#">Some page</a>
+			<a href="#">"Masthead"</a>
+		</nav>
+
+		<nav class="sign-in">
+			<a href="#">Sign-In</a>
+		</nav>
+	</masthead>
+</div>
