a284c0401106a21b381d029c04e4f3fc0ced27f6
Alex Vong <avong003@gmail.com>
2021-06-15 17:07:23 -0700

    index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include('head.php')?>
+
+	<body>
+		<header class="site-header">
+			<?php include('header.php')?>
+		</header>
+		
+		<section class="hero">
+			<?php include('modules/hero-section.php')?>
+		</section>
+		
+		<section class="call-to-action">
+			<?php include('modules/cta.php')?>
+		</section>
+
+		<section class="article-grid">
+			<?php include('modules/article-grid.php')?>
+		</section>
+
+		<section class="call-to-action cta-blue">
+			<?php include('modules/cta.php')?>
+		</section>
+
+		<?php include('footer.php')?>
+		
+	</body>
+</html>
