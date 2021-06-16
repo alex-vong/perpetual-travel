16e2d1f03fc6aaf999b3ef2713571b2b16086d0b
Alex Vong <avong003@gmail.com>
2021-06-15 17:07:07 -0700

    head

---
+++ head.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>Responsive Layout Challenge</title>
+
+	<meta
+		name="og:description"
+		content="Ivy’s Super Ultra Responsive Layout Challenge!"
+	/>
+	<meta property="og:title" content="Alex builds a website" />
+	<meta property="og:image" content="https://peprojects.dev/alpha-1/alexvong/perpetual-travel/images/travel-meta.png" />
+	<link rel="shortcut icon" type="image/jpg" href="images/a-logo.png" />
+
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />	
+</head>
